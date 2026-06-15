6771e9b5de5a9b97fd39023902c5d5971ff35297
divjot121 <aroradivjotsingh@gmail.com>
2026-06-15 14:14:35 +0530

    Added Google Site Verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from "next";
-// import "./global.css";
+import "./globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://divjotsingh.com"),
